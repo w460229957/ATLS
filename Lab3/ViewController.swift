cc73cc734a21efe579eb9995a139b9a3d734c610
Zhengyuan Zhang <meditationzhang@gmail.com>
2021-09-23 02:23:08 -0600

    "Lab3 99% finished"

--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -14,6 +14,49 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var slid: UISlider!
+    @IBOutlet weak var capital: UISwitch!
+    @IBOutlet weak var segmen: UISegmentedControl!
+    @IBOutlet weak var c_picture: UILabel!
+    @IBOutlet weak var c_zhang: UILabel!
+    @IBOutlet weak var cart_girl: UILabel!
+    @IBOutlet weak var cap_name: UILabel!
+    
 
+    
+    @IBAction func changePic(_ sender:UISegmentedControl!) {
+        if(segmen.selectedSegmentIndex == 0){
+            image.image = UIImage(named: "catGirlOne")
+        }
+        else{
+            image.image = UIImage(named: "catGirlTwo")
+        }
+    }
+    func changeFont_Color(_ input:UILabel?,_ condExpr:Int){
+            if(condExpr == 0){
+                input?.text = input?.text?.lowercased();
+                input!.textColor = UIColor.systemYellow;
+            }
+            else{
+                input?.text = input?.text?.uppercased();
+                input!.textColor = UIColor.systemBlue;
+            }
+    }
+    @IBAction func changeFont(_ sender: UISwitch!) {
+        if(sender.isOn){
+            changeFont_Color(c_picture,1);
+            changeFont_Color(c_zhang,1);
+            changeFont_Color(cart_girl,1);
+            changeFont_Color(cap_name,1);
+        }
+        else{
+            changeFont_Color(c_picture,0);
+            changeFont_Color(c_zhang,0);
+            changeFont_Color(cart_girl,0);
+            changeFont_Color(cap_name,0);
+        }
+    }
+    
 }
 
